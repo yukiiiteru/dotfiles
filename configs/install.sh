7e174a6c3f3927d27c2fb4edc8a16ae985596034
Yukiteru <wfly1998@sina.com>
2021-07-04 22:47:52 +0800

    update some scripts and zshrc, kde keybinding

--- configs/install.sh
+++ configs/install.sh
@@ -1,36 +1,40 @@
 #!/bin/sh
 
-function install() {
-	cp ./config_i3 ~/.i3/config
+function install_common() {
 	cp ./profile ~/.profile
 	cp ./xprofile ~/.xprofile
-	cp ./Xresources ~/.Xresources
 	cp ./mimeapps.list ~/.config/mimeapps.list
 	mkdir -p ~/.config/nvim
 	cp ./init.vim ~/.config/nvim/init.vim
 	mkdir -p ~/.local/share/nvim/site/colors
 	cp ./gruvbox.vim ~/.local/share/nvim/site/colors/gruvbox.vim
+	cat ./zshrc >> ~/.zshrc
 	echo "Install finished"
-	exit 0
 }
 
-function escape() {
-	exit 0
+function install_i3() {
+	cp ./config_i3 ~/.i3/config
+	cp ./Xresources ~/.Xresources
 }
 
-read -r -p "Are you sure you want to install ALL OF Yukiteru's config? [y/N] " input
+function main() {
+	read -r -p "Are you sure you want to install ALL OF Yukiteru's config? [y/N] " input
 
-case $input in
-    [yY][eE][sS]|[yY])
-		echo "Yes"
-		;;
+	case $input in
+		[yY][eE][sS]|[yY])
+			install
+			exit 0
+			;;
 
-    [nN][oO]|[nN])
-		escape
-       	;;
+		[nN][oO]|[nN])
+			exit 0
+			;;
 
-    *)
-		escape
-		;;
-esac
+		*)
+			exit 1
+			;;
+	esac
+}
+
+main
 
